6caf5d35314b532d5ae315c719fe87fa875582e8
Diego Escudero <dgerod@xyz-lab.org.es>
2020-10-04 15:48:40 +0200

    Add ariac-server-flatten image to build server images script.

--- ariac-server/build-images.sh
+++ ariac-server/build-images.sh
@@ -10,22 +10,47 @@ set -e
 
 set -x
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [[ $# -lt 1 ]]; then
-  echo "$0 <ros-distro-to-build>"
-  exit 1
-fi
-
+# Constants
 ROS_DISTRO_BUILD_TIME=kinetic
 UBUNTU_DISTRO_TO_BUILD=xenial
 
-docker_images="gzserver gazebo gazebo-ros nvidia-gazebo-ros ariac-server"
+# Arguments
+if [[ $# -lt 1 ]]; then
+  echo "$0 <image-to-build [all|ariac-server-flatten|ariac-server-vnc|...]>"
+  exit 1
+else
+  if [[ $1 == 'all' ]]; then
+    DOCKER_IMAGES="gzserver gazebo gazebo-ros nvidia-gazebo-ros ariac-server ariac-server-flatten ariac-server-vnc"
+  else
+    DOCKER_IMAGES=$1
+  fi
+fi
 
-for docker_img in ${docker_images}; do
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Build all images requested
+for docker_img in ${DOCKER_IMAGES}; do
   echo " Build image: ${docker_img} "
 
   case ${docker_img} in
+    'ariac-server-vnc')
+      DOCKER_ARGS="--no-cache --build-arg ROS_DISTRO_BUILD_TIME=${ROS_DISTRO_BUILD_TIME}"
+      ;;
+    'ariac-server-flatten')
+      USERID=`id -u $USER`
+      # If the script is run by root, do no pass 0 as the USERID to create the
+      # ariac-user. The Dockerfile defaults it to 1000
+      if [[ ${USERID} != 0 ]]; then
+        DOCKER_ARGS="--no-cache \
+                     --build-arg USERID=${USERID} \
+                     --build-arg ROS_DISTRO_BUILD_TIME=${ROS_DISTRO_BUILD_TIME} \
+                     --build-arg UBUNTU_DISTRO_TO_BUILD=${UBUNTU_DISTRO_TO_BUILD}"
+      else 
+        DOCKER_ARGS="--no-cache \
+                     --build-arg ROS_DISTRO_BUILD_TIME=${ROS_DISTRO_BUILD_TIME} \
+                     --build-arg UBUNTU_DISTRO_TO_BUILD=${UBUNTU_DISTRO_TO_BUILD}"
+      fi
+      ;;      
     'ariac-server')
       DOCKER_ARGS="--no-cache --build-arg ROS_DISTRO_BUILD_TIME=${ROS_DISTRO_BUILD_TIME}"
       ;;
@@ -48,14 +73,16 @@ for docker_img in ${docker_images}; do
       DOCKER_ARGS=""
   esac
 
-
-    # Set the proper base image in the Dockerfile according to the ROS distro
-    cp ${DIR}/${docker_img}/Dockerfile_generic \
-       ${DIR}/${docker_img}/Dockerfile
+  # Set the proper base image in the Dockerfile according to the ROS distro
+  cp ${DIR}/${docker_img}/Dockerfile_generic \
+      ${DIR}/${docker_img}/Dockerfile
 
   if [ ${docker_img} = "gzserver" ]; then
     sed -i "s+^FROM.*$+FROM ubuntu:${UBUNTU_DISTRO_TO_BUILD}+" \
       ${DIR}/${docker_img}/Dockerfile
+  #elif [ ${docker_img} = "ariac-server-flatten" ]; then
+  #  sed -i "s+^FROM.*$+FROM ubuntu:${UBUNTU_DISTRO_TO_BUILD}+" \
+  #    ${DIR}/${docker_img}/Dockerfile
   else
     sed -i "s/:latest/-${ROS_DISTRO_BUILD_TIME}:latest/" \
       ${DIR}/${docker_img}/Dockerfile
